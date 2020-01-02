d29b925f017d453e00aa5df5600aa8e8d4cbd4a4
K e n V <kenneth_valdivia@yahoo.com>
2020-01-02 02:34:05 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -36,34 +36,3 @@ def invalid_command
 end
 
 
-describe "#get_user_input" do
-   it "returns the value of a `gets.chomp` method" do
-     ["h", "s", "exit"].each do |string|
-       expect(self).to receive(:gets).and_return(string)
-       expect(get_user_input).to eq(string)
-     end
-   end
- end
-
- describe "#end_game" do
-   it "prints apology, card total, and thank you message" do
-     expect($stdout).to receive(:puts).with("Sorry, you hit 27. Thanks for playing!")
-     end_game(27)
-   end
- end
-
-describe "#end_game" do
-   it "prints apology, card total, and thank you message" do
-     expect($stdout).to receive(:puts).with("Sorry, you hit 27. Thanks for playing!")
-     end_game(27)
-   end
- end
-
- describe "#get_user_input" do
-   it "returns the value of a `gets.chomp` method" do
-     ["h", "s", "exit"].each do |string|
-       expect(self).to receive(:gets).and_return(string)
-       expect(get_user_input).to eq(string)
-     end
-   end
- end
